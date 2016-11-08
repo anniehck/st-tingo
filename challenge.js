6b79f6abd6847b6e823f9cb1ec98fc76aa427340
anniehck <anniehckim@gmail.com>
2016-11-08 13:29:56 -0500

    Locate tagline

--- challenge.js
+++ challenge.js
@@ -2,3 +2,7 @@
 var destination = $('input.destination').val();
 var checkIn = $('input.in_date_field').val();
 var checkOut = $('input.out_date_field').val();
+
+// Task 2
+var tagElement = $('.listing_summary');
+var tagline = tagElement.find('h3').html();
