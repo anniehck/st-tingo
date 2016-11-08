d994a268647d183f3e630b7c956e59fe2a5f3e43
anniehck <anniehckim@gmail.com>
2016-11-08 13:33:15 -0500

    Count current hotels listed

--- challenge.js
+++ challenge.js
@@ -6,3 +6,7 @@ var checkOut = $('input.out_date_field').val();
 // Task 2
 var tagElement = $('.listing_summary');
 var tagline = tagElement.find('h3').html();
+
+var hotelListings = $('#hotel_listings');
+var hotelCount = hotelListings.find('li.tabsParent').length;
+var insertString = hotelCount + ' out of';
