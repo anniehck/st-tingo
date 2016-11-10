2d356c5c57b3efc4a676a5a3ddceb73c42ea1f1f
anniehck <anniehckim@gmail.com>
2016-11-09 19:25:25 -0500

    Wrap in document.ready

--- challenge.js
+++ challenge.js
@@ -4,7 +4,7 @@ var checkIn = $('input.in_date_field').val();
 var checkOut = $('input.out_date_field').val();
 
 $(document).ready(function() {
-  // Task 2
+  // TASK 2
   function replaceTagline(initialLength) {
     var tagline = $('.listing_summary').find('h3').html();
     var hotelCount = $('#hotel_listings').find('li.tabsParent').length;
@@ -26,37 +26,36 @@ $(document).ready(function() {
   replaceTagline(taglineLength);
 
   $(window).scroll(replaceTagline);
-});
 
+  // TASK 3
+    // 3-A
+    $('a.do_show_rates').attr('target', '_blank');
 
-// Task 3
-$('a.do_show_rates').attr('target', '_blank');
+    // 3-B
+    var header = '<div class="header"></div>';
+    var content = '<div class="content"></div>';
+    $('.hotel_name_filter').after('<div class="row" id="selected_hotels">' + header + content + '</div>');
+    var selectedHotels = $('#selected_hotels');
+    var fillerText = '<li><em>Nothing selected yet!</em></li>';
 
-var header = '<div class="header"></div>';
-var content = '<div class="content"></div>';
+    selectedHotels.find('.header').append('<div class="title">Selected Hotels</div>');
+    selectedHotels.find('.content').html('<ul id="selected">' + fillerText + '</ul>').css({'line-height': '1.6em', 'font-size': '.9em'});
+    selectedHotels.find('em').css('color', '#bbb');
 
-$('.hotel_name_filter').after('<div class="row" id="selected_hotels">' + header + content + '</div>');
+    $('a.do_show_rates').click(function() {
+      var hotelInfo = $(this).parent().parent().parent();
+      var name = hotelInfo.find('.title').text();
+      var list = $('ul#selected').find('li');
+      var selected = [];
 
-var selectedHotels = $('#selected_hotels');
-var fillerText = '<li><em>Nothing selected yet!</em></li>';
-
-selectedHotels.find('.header').append('<div class="title">Selected Hotels</div>');
-selectedHotels.find('.content').html('<ul id="selected">' + fillerText + '</ul>').css({'line-height': '1.6em', 'font-size': '.9em'});
-selectedHotels.find('em').css('color', '#bbb');
-
-$('a.do_show_rates').click(function() {
-  var hotelInfo = $(this).parent().parent().parent();
-  var name = hotelInfo.find('.title').text();
-  var list = $('ul#selected').find('li');
-  var selected = [];
-
-  if (list[0].textContent === 'Nothing selected yet!') {
-   list[0].remove();
-  }
-  for (var i = 0; i < list.length; i++) {
-    selected.push(list[i].innerHTML);
-  }
-  if (!selected.includes(name)) {
-    $('ul#selected').append('<li>' + name + '</li>');
-  }
+      if (list[0].textContent === 'Nothing selected yet!') {
+       list[0].remove();
+      }
+      for (var i = 0; i < list.length; i++) {
+        selected.push(list[i].innerHTML);
+      }
+      if (!selected.includes(name)) {
+        $('ul#selected').append('<li>' + name + '</li>');
+      }
+    });
 });
