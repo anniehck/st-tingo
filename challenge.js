063839ae0e5a20eccade6d0140be56dbf4cba578
anniehck <anniehckim@gmail.com>
2016-11-09 19:20:20 -0500

    Refactor task 2

--- challenge.js
+++ challenge.js
@@ -3,30 +3,31 @@ var destination = $('input.destination').val();
 var checkIn = $('input.in_date_field').val();
 var checkOut = $('input.out_date_field').val();
 
-// Task 2
-var tagline = $('.listing_summary').find('h3').html();
-var initial = tagline.length;
-
-var replaceTagline = (initialLength) => {
-  var tagElement = $('.listing_summary');
-  var tagline = tagElement.find('h3').html();
-  var hotelCount = $('#hotel_listings').find('li.tabsParent').length;
-  var split;
-  var resultString;
-  if (tagline.length === initialLength) {
-    var insertString = hotelCount + ' out of';
-    split = tagline.split(' ');
-    split.splice(1, 0, insertString);
-    resultString = split.join(' ');
-  } else {
-    split = tagline.split(' ');
-    split.splice(1, 1, hotelCount);
-    resultString = split.join(' ');
+$(document).ready(function() {
+  // Task 2
+  function replaceTagline(initialLength) {
+    var tagline = $('.listing_summary').find('h3').html();
+    var hotelCount = $('#hotel_listings').find('li.tabsParent').length;
+    var resultString;
+    if (tagline.length === initialLength) {
+      var insertString = hotelCount + ' out of';
+      var split = tagline.split(' ');
+      split.splice(1, 0, insertString);
+      resultString = split.join(' ');
+    } else {
+      var split = tagline.split(' ');
+      split.splice(1, 1, hotelCount);
+      resultString = split.join(' ');
+    }
+    $('.listing_summary').find('h3').html(resultString);
   }
-  tagElement.find('h3').html(resultString);
-}
 
-$(window).on("scroll", replaceTagline(initial), replaceTagline);
+  var taglineLength = $('.listing_summary').find('h3').html().length;
+  replaceTagline(taglineLength);
+
+  $(window).scroll(replaceTagline);
+});
+
 
 // Task 3
 $('a.do_show_rates').attr('target', '_blank');
