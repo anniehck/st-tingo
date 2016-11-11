73a7f57492a43b92ba37fb0f5f545e7a19b0ea76
anniehck <anniehckim@gmail.com>
2016-11-10 22:06:33 -0500

    Refactor

--- challenge.js
+++ challenge.js
@@ -10,20 +10,22 @@ function updateTagline() {
   var hotelCount = $('#hotel_listings').find('li.tabsParent').length;
   var tagWords = tagline.split(' ');
   if (tagWords.includes('Searching')) {
-    // Replace initial tagline
+    // replace initial tagline
     var newTagline = `Showing ${hotelCount} out of`;
     $('.listing_summary > h3').html(tagline.replace('Searching', newTagline));
   } else {
-    // Update just the current hotel count
+    // update just the current hotel count
     tagWords.splice(1, 1, hotelCount);
     $('.listing_summary > h3').html(tagWords.join(' '));
   }
 }
 
+// update tagline, execute func on interval
 updateTagline();
 setInterval(updateTagline, 1000);
 
-// Task 3
+// TASK 3
+// create empty sidebar divs
 var header = '<div class="header"></div>';
 var content = '<div class="content"></div>';
 $('.hotel_name_filter').after(`<div class="row" id="selected_hotels">${header} ${content}</div>`);
@@ -31,27 +33,32 @@ $('.hotel_name_filter').after(`<div class="row" id="selected_hotels">${header} $
 var selectedHotels = $('#selected_hotels');
 var fillerText = '<li><em>Nothing selected yet!</em></li>';
 
+// fill header w title, content w filler text
 selectedHotels.find('.header').append('<div class="title">Selected Hotels</div>');
 selectedHotels.find('.content').html(`<ul id="selected">${fillerText}</ul>`).css({'line-height': '1.7em', 'font-size': '.9em'});
 selectedHotels.find('em').css('color', '#bbb');
 
 $(document).on('click', 'a.do_show_rates', function(e) {
-  // Task 3-A
+  // TASK 3A - open hotel details in new window
   e.preventDefault();
   window.open(e.target.href, '_blank');
 
-  // Task 3-B
+  // TASK 3B - add hotel to sidebar list
+  // find selected hotel name
   var hotelInfo = $(this).parent().parent().parent();
   var name = hotelInfo.find('.title').text();
   var list = $('ul#selected > li');
   var selected = [];
 
+  // remove filler text from list
   if (list[0].textContent === 'Nothing selected yet!') {
    list[0].remove();
   }
+  // add hotel name to array
   for (var i = 0; i < list.length; i++) {
     selected.push(list[i].innerText);
   }
+  // check array to avoid duplication, add hotel link to list
   if (!selected.includes(name)) {
     var link = `<a href="${e.target.href}" target="_blank">${name}</a>`;
     $('ul#selected').append(`<li>${link}</li>`).find('a').css('text-decoration', 'none');
